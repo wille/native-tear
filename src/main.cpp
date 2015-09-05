2fbee1d8108314702cecf8de5abe2f31a34e1d03
redpois0n <red@cmail.nu>
2015-09-05 13:47:56 +0200

    getid() function test (Linux and Windows)

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,24 @@
 
 #include <boost/filesystem/operations.hpp>
 
+#ifdef _WIN32
+#	include <windows.h>
+#	include <Lmcons.h>
+#elif defined(__linux__)
+#	include <pwd.h>
+#endif
+
 using namespace std;
 using namespace boost::filesystem;
 
 void iterate(const path& parent);
 void process(const path& path);
+string getid();
 
 int main(int argc, char* argv[]) {
 	iterate(".");
+
+	cout << "Computer ID: " << getid() << endl;
 	return 0;
 }
 
@@ -31,4 +41,24 @@ void iterate(const path& parent) {
 
 void process(const path& path) {
 	cout << "Processing " << path << endl;
+}
+
+string getid() {
+#ifdef _WIN32
+	char username[UNLEN + 1];
+	DWORD length = UNLEN + 1;
+	GetUserName(username, &length);
+
+	return string(username);
+#elif defined(__linux__)
+	struct passwd *pw;
+
+	uid_t uid = geteuid();
+	pw = getpwuid(uid);
+	if (pw) {
+		return string(pw->pw_name);
+	}
+
+	return to_string(uid);
+#endif
 }
