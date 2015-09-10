31285749b556472124bed71c275ac274b55ab194
redpois0n <red@cmail.nu>
2015-09-10 16:40:17 +0200

    arg1 is key, arg2 is iv (in hex), checks length

--- decrypter/decrypter.cpp
+++ decrypter/decrypter.cpp
@@ -1,6 +1,23 @@
+#include <string>
+#include <iostream>
+
 #include "../native-tear/tear.h"
 
+using namespace std;
+using namespace boost::filesystem;
+using namespace CryptoPP;
+
 int main(int argc, char* argv[]) {
+	string hex_key = string(argv[1]);
+	string hex_iv = string(argv[2]);
+
+	if (hex_key.length() != KEY_LEN * 2) {
+		cerr << "Hex key length must be " << (KEY_LEN * 2) << endl;
+		exit(-1);
+	} else if (hex_iv.length() != IV_LEN * 2) {
+		cerr << "Hex IV length must be " << (IV_LEN * 2) << endl;
+		exit(-1);
+	}
 
 	return 0;
 }
