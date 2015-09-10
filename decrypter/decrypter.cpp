fd910afab1e50deb6c9ac1180e38a466c4446059
redpois0n <red@cmail.nu>
2015-09-10 16:40:51 +0200

    Gets crypt_data

--- decrypter/decrypter.cpp
+++ decrypter/decrypter.cpp
@@ -7,6 +7,8 @@ using namespace std;
 using namespace boost::filesystem;
 using namespace CryptoPP;
 
+crypt_data* get(string key, string iv);
+
 int main(int argc, char* argv[]) {
 	string hex_key = string(argv[1]);
 	string hex_iv = string(argv[2]);
@@ -19,5 +21,23 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 
+	crypt_data* d = get(hex_key, hex_iv);
+
 	return 0;
+}
+
+crypt_data* get(string key, string iv) {
+	crypt_data* d = new crypt_data;
+
+	HexDecoder decoder;
+	decoder.Put((byte*) key.data(), key.size());
+	decoder.MessageEnd();
+	decoder.Get(d->key, sizeof(d->key));
+
+	HexDecoder decoder1;
+	decoder1.Put((byte*) key.data(), key.size());
+	decoder1.MessageEnd();
+	decoder1.Get(d->iv, sizeof(d->iv));
+
+	return d;
 }
