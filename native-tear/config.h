0c54450f01a476ccd80a314f52d10b7bdc29b869
redpois0n <red@cmail.nu>
2015-09-19 17:12:46 +0200

    config with url to panel and comments

--- native-tear/config.h
+++ native-tear/config.h
@@ -1,3 +1,14 @@
+/*
+ * Extension for all locked files
+ */
 #define LOCKED_EXTENSION ".locked"
 
+/*
+ * Key length in bytes, default is 32 (256 bits)
+ */
 #define KEY_LEN 32 // 256 bits
+
+/*
+ * URL to add.php
+ */
+#define URL_PANEL "http://localhost/add.php"
